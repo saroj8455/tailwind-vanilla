7237f9a86e0da6a91b73d5ea50df26222f8493ed
Saroj Padhan <40264980+saroj8455@users.noreply.github.com>
2025-01-12 13:08:46 +0530

    first commit

---
+++ src/main.js
@@ -0,0 +1,5 @@
+import { v6 as uuidv6 } from 'uuid';
+
+console.log('Hello, Tailwind with Vanilla JS!');
+
+console.log(uuidv6());
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    './index.html', // Main HTML file
+    './src/**/*.{js}', // Include all JavaScript files
+  ],
+  theme: {
+    extend: {}, // Add custom configurations if needed
+  },
+  plugins: [],
+};
