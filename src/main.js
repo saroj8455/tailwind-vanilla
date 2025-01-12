2fcfd6e00360eef8c02d84c10d52b5e37706c0b8
Saroj Padhan <40264980+saroj8455@users.noreply.github.com>
2025-01-12 14:17:16 +0530

    implement trueLength using prototype in string

--- src/main.js
+++ src/main.js
@@ -2,4 +2,15 @@ import { v6 as uuidv6 } from 'uuid';
 
 console.log('Hello, Tailwind with Vanilla JS!');
 
-console.log(uuidv6());
+// Implement the feature of prototype
+String.prototype.trueLength = function () {
+  if (typeof this !== 'string')
+    throw new TypeError('trueLength can only be called on strings.');
+  // Use a regex to remove non-alphanumeric characters and return the length
+  return this.replace(/[^a-zA-Z0-9]/g, '').length;
+};
+
+const msg = 'Hi, hello world app';
+const greet = '    hello   ';
+console.log(msg.trueLength());
+console.log(greet.trueLength());
